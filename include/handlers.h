258b3245cf553e86aa86f65bd61d73cbf2de2dbe
tmalkawi02 <amaryllismain09@gmail.com>
2025-10-08 16:29:33 +0200

    add k release and k press button functions

---
+++ include/handlers.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   handlers.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tmalkawi <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/08 16:05:37 by tmalkawi          #+#    #+#             */
+/*   Updated: 2025/10/08 16:05:37 by tmalkawi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HANDLERS_H
+# define HANDLERS_H
+
+# include "raycasters.h"
+
+int	k_release(int keytcode, t_game *game);
+
+int	k_press(int keycode, t_game *game);
+
+#endif
