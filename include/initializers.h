b5ea2c0c562bebc65fa2c776581893d89042572d
tmalkawi02 <amaryllismain09@gmail.com>
2025-10-08 15:00:21 +0200

    chore: add initializers.h

---
+++ include/initializers.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   initializers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tmalkawi <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/08 14:58:08 by tmalkawi          #+#    #+#             */
+/*   Updated: 2025/10/08 14:58:08 by tmalkawi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef INITIALIZERS_H
+# define INITIALIZERS_H
+
+# include "raycasters.h"
+
+void	init_game(t_game *game);
+
+void	init_cam(t_game *game);
+
+void	init_textures(t_game *game);
+
+#endif // !INITIALIZERS_H
