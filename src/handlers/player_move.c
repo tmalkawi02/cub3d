03bdad4203281b432f16bfad4b4e5e4ff69ae066
tmalkawi02 <amaryllismain09@gmail.com>
2025-10-14 11:15:54 +0200

    chore: define player_move function

---
+++ src/handlers/player_move.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   player_move.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tmalkawi <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/14 11:10:33 by tmalkawi          #+#    #+#             */
+/*   Updated: 2025/10/14 11:10:33 by tmalkawi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "handlers.h"
+
+void	player_move(t_game *game)
+{
+	if (game->play->kright)
+		player_move_right(game);
+	else if (game->play->kleft)
+		player_move_left(game);
+	else if (game->play->kup)
+		player_move_up(game);
+	else if (game->play->kdown)
+		player_move_down(game);
+	else if (game->play->rrotate)
+		player_rotate_right(game);
+	else if (game->play->lrotate)
+		player_rotate_left(game);
+	else
+		return ;
+	return ;
+}
