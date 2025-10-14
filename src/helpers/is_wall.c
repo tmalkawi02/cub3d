f20f221a128829dd8f5422b2136a91118f9f2ccb
tmalkawi02 <amaryllismain09@gmail.com>
2025-10-14 15:05:23 +0200

    chore: add is_wall file

---
+++ src/helpers/is_wall.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   is_wall.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tmalkawi <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/14 14:47:48 by tmalkawi          #+#    #+#             */
+/*   Updated: 2025/10/14 14:47:48 by tmalkawi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "helpers.h"
+#include <stdbool.h>
+
+// TODO: add map metrics bounds
+bool	is_wall(t_game *game, int x, int y)
+{
+	char	c;
+
+	if (x < 0 || y < 0)
+		return (true);
+	c = game->map[y][x];
+	if (c == '0' || c == 'N' || c == 'S' || c == 'E' || c == 'W')
+		return (false);
+	return (true);
+}
