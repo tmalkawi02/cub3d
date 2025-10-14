2ba9f4933fb32b0057d9836a7a114f23b570d3c9
tmalkawi02 <amaryllismain09@gmail.com>
2025-10-14 12:09:19 +0200

    feat: define player_move_left function

---
+++ src/handlers/player_move_left.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   player_move_left.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tmalkawi <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/14 12:05:56 by tmalkawi          #+#    #+#             */
+/*   Updated: 2025/10/14 12:05:56 by tmalkawi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "handlers.h"
+#include "raycasters.h"
+
+void	player_move_left(t_game *game)
+{
+	float	new_x;
+	float	new_y;
+
+	new_x = game->play->pos_x - game->play->dir_y * MOVE_SPEED;
+	new_y = game->play->pos_y + game->play->dir_x * MOVE_SPEED;
+	if (game->map[(int)new_y][(int)game->play->pos_x] == 0)
+		game->play->pos_y = new_y;
+	if (game->map[(int)game->play->pos_y][(int)new_x])
+		game->play->pos_x = new_x;
+}
