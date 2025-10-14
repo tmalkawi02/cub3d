f5e3a97e33c60e3f1c5289cc52113c163827b974
tmalkawi02 <amaryllismain09@gmail.com>
2025-10-14 16:36:43 +0200

    feat: define player rotation left and right

--- src/handlers/player_rotate_left.c
+++ src/handlers/player_rotate_left.c
@@ -11,9 +11,23 @@
 /* ************************************************************************** */
 
 #include "handlers.h"
+#include "raycasters.h"
+#include <math.h>
 
 void	player_rotate_left(t_game *game)
 {
-	(void) game;
+	float	old_dir_x;
+	float	old_plane_x;
+
+	old_dir_x = game->play->dir_x;
+	old_plane_x = game->play->plane_x;
+	game->play->dir_x = game->play->dir_x * cos(ROT_SPEED)
+		- game->play->dir_y * sin(ROT_SPEED);
+	game->play->dir_y = old_dir_x * sin(ROT_SPEED)
+		+ game->play->dir_y * cos(ROT_SPEED);
+	game->play->plane_x = game->play->plane_x * cos(ROT_SPEED)
+		- game->play->plane_y * sin(ROT_SPEED);
+	game->play->plane_y = old_plane_x * sin(ROT_SPEED)
+		+ game->play->plane_y * cos(ROT_SPEED);
 	return ;
 }
--- src/handlers/player_rotate_right.c
+++ src/handlers/player_rotate_right.c
@@ -11,9 +11,25 @@
 /* ************************************************************************** */
 
 #include "handlers.h"
+#include "raycasters.h"
+#include <math.h>
 
 void	player_rotate_right(t_game *game)
 {
-	(void) game;
+	float	old_dir_x;
+	float	old_plane_x;
+	float	angle;
+
+	old_dir_x = game->play->dir_x;
+	old_plane_x = game->play->dir_y;
+	angle = -ROT_SPEED;
+	game->play->dir_x = game->play->dir_x * cos(angle)
+		- game->play->dir_y * sin(angle);
+	game->play->dir_y = old_dir_x * sin(angle)
+		+ game->play->dir_y * cos(angle);
+	game->play->plane_x = game->play->plane_x * cos(angle)
+		- game->play->plane_y * sin(angle);
+	game->play->plane_y = old_plane_x * sin(angle)
+		+ game->play->plane_y * cos(angle);
 	return ;
 }
