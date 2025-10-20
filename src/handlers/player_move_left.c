71106d9bdbbd315722ffad8308e9510938a3d8fa
tmalkawi02 <amaryllismain09@gmail.com>
2025-10-20 12:27:30 +0200

    fix: bad coords

--- src/handlers/player_move_left.c
+++ src/handlers/player_move_left.c
@@ -13,16 +13,17 @@
 #include "handlers.h"
 #include "raycasters.h"
 #include "helpers.h"
+#include <stdio.h>
 
 void	player_move_left(t_game *game)
 {
 	float	new_x;
 	float	new_y;
 
-	new_x = game->play->pos_x - game->play->dir_y * MOVE_SPEED;
-	new_y = game->play->pos_y + game->play->dir_x * MOVE_SPEED;
-	if (!is_wall(game, (int)new_y, (int)game->play->pos_x))
+	new_x = game->play->pos_x + game->play->dir_y * MOVE_SPEED;
+	new_y = game->play->pos_y - game->play->dir_x * MOVE_SPEED;
+	if (!is_wall(game, (int)game->play->pos_x, (int)new_y))
 		game->play->pos_y = new_y;
-	if (!is_wall(game, (int)game->play->pos_y, new_x))
+	if (!is_wall(game, (int)new_x, (int)game->play->pos_y))
 		game->play->pos_x = new_x;
 }
