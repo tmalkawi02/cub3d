d601f8400f7eb02cbd635b308c1984a30bd14d20
tmalkawi02 <amaryllismain09@gmail.com>
2025-10-20 13:48:36 +0200

    fix: invert rotate angles

--- src/handlers/player_rotate_right.c
+++ src/handlers/player_rotate_right.c
@@ -18,18 +18,16 @@ void	player_rotate_right(t_game *game)
 {
 	float	old_dir_x;
 	float	old_plane_x;
-	float	angle;
 
 	old_dir_x = game->play->dir_x;
-	old_plane_x = game->play->dir_y;
-	angle = -ROT_SPEED;
-	game->play->dir_x = game->play->dir_x * cos(angle)
-		- game->play->dir_y * sin(angle);
-	game->play->dir_y = old_dir_x * sin(angle)
-		+ game->play->dir_y * cos(angle);
-	game->play->plane_x = game->play->plane_x * cos(angle)
-		- game->play->plane_y * sin(angle);
-	game->play->plane_y = old_plane_x * sin(angle)
-		+ game->play->plane_y * cos(angle);
+	old_plane_x = game->play->plane_x;
+	game->play->dir_x = game->play->dir_x * cos(ROT_SPEED)
+		- game->play->dir_y * sin(ROT_SPEED);
+	game->play->dir_y = old_dir_x * sin(ROT_SPEED)
+		+ game->play->dir_y * cos(ROT_SPEED);
+	game->play->plane_x = game->play->plane_x * cos(ROT_SPEED)
+		- game->play->plane_y * sin(ROT_SPEED);
+	game->play->plane_y = old_plane_x * sin(ROT_SPEED)
+		+ game->play->plane_y * cos(ROT_SPEED);
 	return ;
 }
