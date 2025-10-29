0c994a2767927bfee26fc0f46366f68dc694e199
aborel <aborel@student.42.fr>
2025-10-29 16:19:30 +0100

    is_wall: max x et y fixed

--- src/helpers/is_wall.c
+++ src/helpers/is_wall.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   is_wall.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tmalkawi <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: aborel <aborel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/14 14:47:48 by tmalkawi          #+#    #+#             */
-/*   Updated: 2025/10/14 14:47:48 by tmalkawi         ###   ########.fr       */
+/*   Updated: 2025/10/29 15:36:59 by aborel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ bool	is_wall(t_game *game, int x, int y)
 {
 	char	c;
 
-	if (x < 0 || y < 0 || x > 32 || y > 13)
+	if (x < 0 || y < 0 || x > game->n_cols || y > game->n_rows)
 		return (true);
 	c = game->map[y][x];
 	if (c == '0' || c == 'N' || c == 'S' || c == 'E' || c == 'W')
