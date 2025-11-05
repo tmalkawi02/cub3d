7c044575d59b841a343be11b6b7717cb7b381881
tmalkawi02 <amaryllismain09@gmail.com>
2025-11-05 12:59:09 +0100

    feat: flip texture coords accordingly

--- src/raycasters/calculate_texture_coord.c
+++ src/raycasters/calculate_texture_coord.c
@@ -13,16 +13,15 @@
 #include "raycasters.h"
 #include <math.h>
 
-void	calculate_texture_coord(t_ray *ray,
-			t_wall *wall, t_texture *tex)
+void	calculate_texture_coord(t_ray *ray, t_wall *wall, t_texture *tex)
 {
 	wall->tex_x = (int)(wall->wall_x * (double)tex->width);
 	if (wall->tex_x < 0)
 		wall->tex_x = 0;
 	if (wall->tex_x >= tex->width)
 		wall->tex_x = tex->width - 1;
-	if ((ray->side == 0 && ray->dir_x > 0)
-		|| (ray->side == 1 && ray->dir_y < 0))
+	if ((ray->side == 0 && ray->dir_x < 0)
+		|| (ray->side == 1 && ray->dir_y > 0))
 		wall->tex_x = tex->width - wall->tex_x - 1;
 	wall->step = (double)tex->height / (double)wall->line_height;
 	wall->tex_pos = (wall->draw_start - (double)WIN_HEIGHT / 2.0
